e21bb5ee3af4b1ee5ed87aa010225073999d0fdb
Stephen Engler <sengler1989@gmail.com>
2018-06-20 20:11:18 -0500

    if else

---
+++ ifelse.py
@@ -0,0 +1,15 @@
+#if something:
+hungry = True
+if hungry:
+    print('its true')
+else:
+    print('its false')
+
+loc = 'bank'
+
+if loc == 'coffee shop':
+    print('coffee')
+elif loc == 'store':
+    print('store')
+else:
+    print('no where')
